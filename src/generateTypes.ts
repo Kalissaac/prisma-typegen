7ddfdc2ba627b2433c870457416d7949f3f15a24
Kalissaac <kalissaac@protonmail.com>
2021-09-15 15:33:05 -0700

    Add initial generator

---
+++ src/generateTypes.ts
@@ -0,0 +1,111 @@
+import { getDMMF } from '@prisma/sdk'
+import { mkdirSync, writeFileSync } from 'fs'
+import { join } from 'path'
+import type { DMMF } from '@prisma/generator-helper';
+
+interface TypeTransfer {
+  models: Model[]
+  enums: Enum[]
+}
+
+interface Model {
+  name: string
+  fields: {
+    name: string
+    typeAnnotation: string
+  }[]
+}
+
+interface Enum {
+  name: string
+  values: string[]
+}
+
+export default async function generateTypes (schemaPath: string, outputPath: string) {
+  const dmmf = await getDMMF({ datamodelPath: schemaPath })
+  let types = distillDMMF(dmmf)
+  types = convertPrismaTypesToJSTypes(types)
+  const fileContents = createTypeFileContents(types)
+  writeToFile(fileContents, outputPath)
+}
+
+function distillDMMF (dmmf: DMMF.Document): TypeTransfer {
+  const types: TypeTransfer = {
+    models: [],
+    enums: []
+  }
+
+  dmmf.datamodel.enums.forEach(prismaEnum => {
+    types.enums.push({
+      name: prismaEnum.name,
+      values: prismaEnum.values.map(e => e.name)
+    })
+  })
+
+  dmmf.datamodel.models.forEach(model => {
+    types.models.push({
+      name: model.name,
+      fields: model.fields.map(f => ({
+        name: f.name,
+        typeAnnotation: f.type
+      }))
+    })
+  })
+
+  return types
+}
+
+const PrismaTypesMap = new Map([
+  ['String', 'string'],
+  ['Boolean', 'boolean'],
+  ['Int', 'number'],
+  ['BigInt', 'number'],
+  ['Float', 'number'],
+  ['Decimal', 'number'],
+  ['DateTime', 'Date'],
+  ['Json', 'any'],
+  ['Bytes', 'Buffer']
+])
+
+function convertPrismaTypesToJSTypes (types: TypeTransfer): TypeTransfer {
+  const models = types.models.map(model => {
+    const fields = model.fields.map(field => ({
+      ...field,
+      typeAnnotation: PrismaTypesMap.get(field.typeAnnotation) || field.typeAnnotation
+    }))
+
+    return {
+      ...model,
+      fields
+    }
+  })
+
+  return {
+    ...types,
+    models
+  }
+}
+
+function createTypeFileContents (types: TypeTransfer): string {
+  let fileContents = `// AUTO GENERATED FILE BY @kalissaac/prisma-typegen
+// DO NOT EDIT
+
+${types.enums.map(prismaEnum => `
+export enum ${prismaEnum.name} {
+${prismaEnum.values.map(value => `    ${value},`).join('\n')}
+}`).join('\n')}
+
+${types.models.map(model => `
+export interface ${model.name} {
+${model.fields.map(field => `    ${field.name}: ${field.typeAnnotation},`).join('\n')}
+}`).join('\n')}
+`
+  return fileContents
+}
+
+async function writeToFile (contents: string, outputPath: string) {
+  mkdirSync(outputPath, { recursive: true })
+  writeFileSync(join(outputPath, 'index.d.ts'), contents, {
+    encoding: 'utf8'
+  })
+}
--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,55 @@
-import {Command, flags} from '@oclif/command'
-import { getDMMF } from '@prisma/sdk'
+import { Command, flags } from '@oclif/command'
+import generateTypes from './generateTypes'
+import cli from 'cli-ux'
 
 class PrismaTypegen extends Command {
-  static description = 'describe the command here'
+  static description = `Generates full types (including relations) for TypeScript from a Prisma schema
+e.g. npx @kalissaac/prisma-typegen ./interfaces/prisma ./schema.prisma`
 
   static flags = {
     // add --version flag to show CLI version
-    version: flags.version({char: 'v'}),
-    help: flags.help({char: 'h'}),
-    // flag with a value (-n, --name=VALUE)
-    name: flags.string({char: 'n', description: 'name to print'}),
-    // flag with no value (-f, --force)
-    force: flags.boolean({char: 'f'}),
+    version: flags.version({ char: 'v' }),
+    help: flags.help({ char: 'h' }),
   }
 
-  static args = [{name: 'file'}]
-
-  async run() {
-    const {args, flags} = this.parse(PrismaTypegen)
-
-    const name = flags.name ?? 'world'
-    this.log(`hello ${name} from ./src/index.ts`)
-    if (args.file && flags.force) {
-      this.log(`you input --force and --file: ${args.file}`)
+  static args = [
+    {
+      name: 'outputPath',
+      required: true,
+      description: 'Path to output the generated types (the index.d.ts file)',
+    },
+    {
+      name: 'schema',
+      required: false,
+      description: 'Path to the schema file (schema.prisma)',
     }
-    const dmmf = await getDMMF({ datamodel: "model User { id String @id }" })
+  ]
+
+  async run () {
+    const { args } = this.parse(PrismaTypegen)
+
+    let outputPath = args.outputPath
+    let schemaLocation = args.schema
+
+    if (!outputPath) {
+      this.error('Output path is required')
+    }
+    if (!schemaLocation) {
+      cli.action.start('Looking for schema.prisma')
+
+      cli.action.stop()
+      this.error('Schema file is required and could not be automatically found')
+    }
+
+    try {
+      cli.action.start('Generating types')
+      await generateTypes(schemaLocation, outputPath)
+      cli.action.stop()
+      this.log('Done!')
+    } catch (e) {
+      cli.action.stop()
+      this.error(e as string)
+    }
   }
 }
 
